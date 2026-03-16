e29c18ffd740a60236b83be3a41cdfa42138f438
YuriLucas7 <lucasy973@gmail.com>
2026-03-16 14:49:14 -0300

    correcao model

--- Zenite.InventarioVegetal.Server/models/Historico.cs
+++ Zenite.InventarioVegetal.Server/models/Historico.cs
@@ -2,9 +2,10 @@
 {
     public class Historico
     {
+        public int Id { get; set; }
         public int IdItemEstoque { get; set; }
-        public int IdUsuario { get; set; }
         public TipoMovimentacao TipoMovimentacao { get; set; }
+        public int IdUsuario { get; set; }
         public DateTime DataHora { get; set; } = DateTime.UtcNow;
         public decimal QuantidadeAnterior { get; set; }
         public decimal QuantidadeResultante { get; set; }
