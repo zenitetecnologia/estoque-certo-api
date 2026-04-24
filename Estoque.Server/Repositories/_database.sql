11e77ff28d8fd284fd019a7cf4eefa72b1ded213
YuriLucas7 <lucasy973@gmail.com>
2026-04-24 12:30:16 -0300

    att _databasesql

--- Estoque.Server/Repositories/_database.sql
+++ Estoque.Server/Repositories/_database.sql
@@ -12,36 +12,37 @@ CREATE TABLE IF NOT EXISTS estoque_certo.unidade_organizacional (
     razao_social VARCHAR(100) NOT NULL,
     nome_fantasia VARCHAR(100),
     cep VARCHAR(8),
-    endereco VARCHAR(200),
+    endereco VARCHAR(100),
     numero VARCHAR(50),
     complemento VARCHAR(50),
     bairro VARCHAR(100),
     cidade VARCHAR(100),
     uf VARCHAR(2),
     pais VARCHAR(100),
-    email VARCHAR(100)
+    email VARCHAR(100),
+    telefone VARCHAR(12)
 );
 
--- 4. Tabela de Espaços (Locais físicos de armazenamento)
+-- 3. Tabela de Espaços (Locais físicos de armazenamento)
 CREATE TABLE IF NOT EXISTS estoque_certo.espaco (
     espaco_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     unidade_organizacional_id UUID NOT NULL,
-    nome VARCHAR(150) NOT NULL,
-    descricao TEXT,
+    nome VARCHAR(100) NOT NULL,
+    descricao VARCHAR(255),
     
     CONSTRAINT fk_espaco_unidade FOREIGN KEY (unidade_organizacional_id) 
         REFERENCES estoque_certo.unidade_organizacional (unidade_organizacional_id) ON DELETE CASCADE
 );
 
 
--- 3. Tabela de Usuários (Relação 1:N direta com Unidade Organizacional)
+-- 4. Tabela de Usuários (Relação 1:N direta com Unidade Organizacional)
 CREATE TABLE IF NOT EXISTS estoque_certo.usuario (
     usuario_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    username VARCHAR(32) NOT NULL,
-    senha VARCHAR(36) NOT NULL,
+    unidade_organizacional_id UUID NOT NULL,
+    username VARCHAR(12) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
     nome VARCHAR(100) NOT NULL,
     perfil INTEGER NOT NULL,
-    unidade_organizacional_id UUID NOT NULL,
     valido BOOLEAN NOT NULL DEFAULT FALSE,
     
     -- Restrição que permite o mesmo username apenas se for em unidades diferentes
@@ -56,15 +57,14 @@ CREATE TABLE IF NOT EXISTS estoque_certo.usuario (
 CREATE TABLE IF NOT EXISTS estoque_certo.item_estoque (
     item_estoque_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     unidade_organizacional_id UUID NOT NULL,
-    espaco_id UUID NOT NULL, -- CORREÇÃO: Alterado de 'espaco' para 'espaco_id'
-    descricao TEXT NOT NULL,
-    tipo_unidade_medida INTEGER NOT NULL, -- Enum mantido como INTEGER
-    quantidade NUMERIC(18,4) NOT NULL DEFAULT 0,
+    espaco_id UUID NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    tipo_unidade_medida INTEGER NOT NULL,
+    quantidade NUMERIC(18,3) NOT NULL DEFAULT 0,
     
     CONSTRAINT fk_item_unidade FOREIGN KEY (unidade_organizacional_id) 
         REFERENCES estoque_certo.unidade_organizacional (unidade_organizacional_id) ON DELETE CASCADE,
         
-    -- Nova chave estrangeira atualizada para usar espaco_id
     CONSTRAINT fk_item_espaco FOREIGN KEY (espaco_id) 
         REFERENCES estoque_certo.espaco (espaco_id) ON DELETE RESTRICT
 );
@@ -73,11 +73,11 @@ CREATE TABLE IF NOT EXISTS estoque_certo.item_estoque (
 CREATE TABLE IF NOT EXISTS estoque_certo.historico (
     historico_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     item_estoque_id UUID NOT NULL,
-    tipo_movimentacao INTEGER NOT NULL, -- Enum mantido como INTEGER
     usuario_id UUID,
+    tipo_movimentacao INTEGER NOT NULL,
     data_hora TIMESTAMP NOT NULL DEFAULT NOW(),
-    quantidade_anterior NUMERIC(18,4) NOT NULL,
-    quantidade_resultante NUMERIC(18,4) NOT NULL,
+    quantidade_anterior NUMERIC(18,3) NOT NULL,
+    quantidade_resultante NUMERIC(18,3) NOT NULL,
     
     CONSTRAINT fk_historico_item FOREIGN KEY (item_estoque_id) 
         REFERENCES estoque_certo.item_estoque (item_estoque_id) ON DELETE CASCADE,
@@ -86,70 +86,13 @@ CREATE TABLE IF NOT EXISTS estoque_certo.historico (
         REFERENCES estoque_certo.usuario (usuario_id) ON DELETE SET NULL
 );
 
-INSERT INTO estoque_certo.unidade_organizacional
-(
-unidade_organizacional_id,
-razao_social,
-nome_fantasia,
-cnpj
-)
-VALUES
-(
-'e286f99e-5075-4b73-96d9-af462c36a5a6',
-'Zênite Tecnologia LTDA',
-'Zênite Tecnologia',
-'60935686000134'
-);
-
-INSERT INTO estoque_certo.espaco
-(
-espaco_id,
-unidade_organizacional_id,
-nome,
-descricao
-)
-VALUES
-(
-'6ab71fef-cf4e-4dbf-af76-f2058224721a',
-'e286f99e-5075-4b73-96d9-af462c36a5a6',
-'Sala 307',
-'Sede Moema'
-);
-
-INSERT INTO estoque_certo.usuario
-(
-usuario_id,
-username,
-senha,
-nome,
-perfil,
-unidade_organizacional_id,
-valido
-)
-VALUES
-(
-'bcfd8e5d-ccdb-46cf-bfe2-da19e5514981',
-'teste_de_username_duplicado',
-'insomnia',
-'teste',
-'1',
-'e286f99e-5075-4b73-96d9-af462c36a5a6',
-'true'
-);
-
-insert into estoque_certo.item_estoque 
-(
-	unidade_organizacional_id,
-	espaco_id,
-	descricao,
-	tipo_unidade_medida,
-	quantidade
-)
-values 
-(
-	'e286f99e-5075-4b73-96d9-af462c36a5a6',
-	'6ab71fef-cf4e-4dbf-af76-f2058224721a',
-	'bala de caramelo',
-	'1',
-	'10'
+-- 7. Tabela de Auth (Autenticação e Autorização)
+CREATE TABLE estoque_certo.codigo_acesso (
+    usuario_id UUID NOT NULL REFERENCES estoque_certo.usuario(usuario_id),
+    codigo VARCHAR(6) NOT NULL,
+    data_solicitacao TIMESTAMP NOT NULL DEFAULT NOW(),
+    data_validacao TIMESTAMP NULL,
+    validado BOOLEAN NOT NULL DEFAULT FALSE,
+    codigo_acesso_id VARCHAR(100) NULL,
+    PRIMARY KEY (usuario_id, codigo)
 );
