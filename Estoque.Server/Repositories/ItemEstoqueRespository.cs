34be3f3b1402dc59394968a69206477970e2a972
YuriLucas7 <lucasy973@gmail.com>
2026-05-18 16:47:38 -0300

    feature: transferir itens de espacos

--- Estoque.Server/Repositories/ItemEstoqueRespository.cs
+++ Estoque.Server/Repositories/ItemEstoqueRespository.cs
@@ -269,4 +269,32 @@ public class ItemEstoqueRepository : BaseRepository
             throw;
         }
     }
+
+    public async Task<int> TransferirEspaco(Guid itemEstoqueId, Guid novoEspacoId)
+    {
+        const string sql = @"
+            UPDATE
+                estoque_certo.item_estoque
+            SET
+                espaco_id = @novo_espaco_id
+            WHERE
+                item_estoque_id = @item_estoque_id;
+        ";
+
+        try
+        {
+            await EnsureOpenAsync();
+
+            await using var cmd = new NpgsqlCommand(sql, Connection);
+
+            cmd.Parameters.Add("item_estoque_id", NpgsqlDbType.Uuid).Value = itemEstoqueId;
+            cmd.Parameters.Add("novo_espaco_id", NpgsqlDbType.Uuid).Value = novoEspacoId;
+
+            return await cmd.ExecuteNonQueryAsync();
+        }
+        catch
+        {
+            throw;
+        }
+    }
 }
